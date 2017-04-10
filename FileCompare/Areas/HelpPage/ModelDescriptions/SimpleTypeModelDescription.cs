20cc6076876c299095e44271f3ac95f782be2971
dmitriyd <dmytro_dubovyk@ukr.net>
2017-04-10 11:16:59 +0300

    Initial commit

---
+++ FileCompare/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FileCompare.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ FileCompare/Models/TutorialModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial.FileUpload.Models
+{
+    public class FileCompareModel
+    {
+		public HttpPostedFileBase Attachment1 { get; set; }
+		public HttpPostedFileBase Attachment2 { get; set; }
+	}
+}
